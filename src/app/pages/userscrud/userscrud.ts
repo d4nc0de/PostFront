0000a4fb6094ada19cd082a47b31a1664368f5df
DevJeffreyG <gabrielpaez.r@hotmail.com>
2025-11-10 22:25:04 -0500

    CRUD Usuarios
    + eliminacion de plantillas sin uso

--- src/app/pages/userscrud/userscrud.ts
+++ src/app/pages/userscrud/userscrud.ts
@@ -1,225 +1,184 @@
-import { Component, OnInit, signal, ViewChild } from '@angular/core';
-import { ConfirmationService, MessageService } from 'primeng/api';
-import { Table, TableModule } from 'primeng/table';
 import { CommonModule } from '@angular/common';
+import { Component, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ConfirmationService, MessageService } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { DialogModule } from 'primeng/dialog';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { InputTextModule } from 'primeng/inputtext';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { RatingModule } from 'primeng/rating';
 import { RippleModule } from 'primeng/ripple';
+import { SelectModule } from 'primeng/select';
+import { Table, TableModule } from 'primeng/table';
+import { TagModule } from 'primeng/tag';
+import { TextareaModule } from 'primeng/textarea';
 import { ToastModule } from 'primeng/toast';
 import { ToolbarModule } from 'primeng/toolbar';
-import { RatingModule } from 'primeng/rating';
-import { InputTextModule } from 'primeng/inputtext';
-import { TextareaModule } from 'primeng/textarea';
-import { SelectModule } from 'primeng/select';
-import { RadioButtonModule } from 'primeng/radiobutton';
-import { InputNumberModule } from 'primeng/inputnumber';
-import { DialogModule } from 'primeng/dialog';
-import { TagModule } from 'primeng/tag';
-import { InputIconModule } from 'primeng/inputicon';
-import { IconFieldModule } from 'primeng/iconfield';
-import { ConfirmDialogModule } from 'primeng/confirmdialog';
-import { Product, ProductService } from '../service/product.service';
+import { BookService } from '../service/books.service';
+import { User } from '@/Models/user.model';
+import { UserService } from '../service/users.service';
 
 interface Column {
-    field: string;
-    header: string;
-    customExportHeader?: string;
-}
-
-interface ExportColumn {
-    title: string;
-    dataKey: string;
+  field: string;
+  header: string;
+  customExportHeader?: string;
 }
 
 @Component({
-  selector: 'app-userscrud',
-  imports: [
-        CommonModule,
-        TableModule,
-        FormsModule,
-        ButtonModule,
-        RippleModule,
-        ToastModule,
-        ToolbarModule,
-        RatingModule,
-        InputTextModule,
-        TextareaModule,
-        SelectModule,
-        RadioButtonModule,
-        InputNumberModule,
-        DialogModule,
-        TagModule,
-        InputIconModule,
-        IconFieldModule,
-        ConfirmDialogModule],
-  providers: [MessageService, ProductService, ConfirmationService],
+  selector: 'app-authors-crud',
+  imports: [CommonModule,
+    TableModule,
+    FormsModule,
+    ButtonModule,
+    RippleModule,
+    ToastModule,
+    ToolbarModule,
+    RatingModule,
+    InputTextModule,
+    TextareaModule,
+    SelectModule,
+    RadioButtonModule,
+    InputNumberModule,
+    DialogModule,
+    TagModule,
+    InputIconModule,
+    IconFieldModule,
+    ConfirmDialogModule],
+  providers: [MessageService, BookService, ConfirmationService],
   templateUrl: './userscrud.html',
   styleUrl: './userscrud.scss'
 })
+export class Userscrud {
+  userDialog: boolean = false;
+  isNew: boolean = false;
 
-export class Userscrud implements OnInit {
-    productDialog: boolean = false;
+  users: User[] = [];
 
-    products = signal<Product[]>([]);
+  user!: User;
+  unchangedUser!: User;
 
-    product!: Product;
+  selectedUsers!: User[] | null;
 
-    selectedProducts!: Product[] | null;
+  submitted: boolean = false;
 
-    submitted: boolean = false;
+  statuses!: any[];
 
-    statuses!: any[];
+  @ViewChild('dt') dt!: Table;
 
-    @ViewChild('dt') dt!: Table;
+  cols!: Column[];
 
-    exportColumns!: ExportColumn[];
+  constructor(
+    private usersService: UserService,
+    private messageService: MessageService,
+    private confirmationService: ConfirmationService
+  ) { }
 
-    cols!: Column[];
+  ngOnInit() {
+    // Suscribirse al Observable para obtener los posts de la API
+    this.usersService.getUsers().subscribe({
+      next: (users) => {
+        console.log('✅ Usuarios obtenidos de la API:', users);
+        this.users = users;
+      },
+      error: (error) => {
+        console.error('❌ Error al obtener usuarios:', error);
+      }
+    });
+  }
 
-    constructor(
-        private productService: ProductService,
-        private messageService: MessageService,
-        private confirmationService: ConfirmationService
-    ) {}
+  onGlobalFilter(table: Table, event: Event) {
+    table.filterGlobal((event.target as HTMLInputElement).value, 'contains');
+  }
 
-    exportCSV() {
-        this.dt.exportCSV();
-    }
+  openNew() {
+    this.user = { userName: '', id: 0, email: '', password: '' };
+    this.submitted = false;
+    this.userDialog = true;
+    this.isNew = true;
+  }
 
-    ngOnInit() {
-        this.loadDemoData();
-    }
+  editUser(user: User) {
+    this.isNew = false;
+    this.unchangedUser = { ...user }; // Store the original user for comparison
+    this.user = structuredClone(user);
+    this.userDialog = true;
+  }
 
-    loadDemoData() {
-        this.productService.getProducts().then((data) => {
-            this.products.set(data);
+  deleteSelectedUsers() {
+    this.confirmationService.confirm({
+      message: 'Are you sure you want to delete the selected users?',
+      header: 'Confirm',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+        // TODO: Eliminacion de usuarios API
+        this.selectedUsers = null;
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Successful',
+          detail: 'Users Deleted',
+          life: 3000
         });
+      }
+    });
+  }
 
-        this.cols = [
-            { field: 'idu', header: 'Id Usuario', customExportHeader: 'idu' },
-            { field: 'nombre', header: 'Nombre' }
-        ];
+  hideDialog() {
+    this.userDialog = false;
+    this.submitted = false;
+  }
 
-        this.exportColumns = this.cols.map((col) => ({ title: col.header, dataKey: col.field }));
-    }
-
-    onGlobalFilter(table: Table, event: Event) {
-        table.filterGlobal((event.target as HTMLInputElement).value, 'contains');
-    }
-
-    openNew() {
-        this.product = {};
-        this.submitted = false;
-        this.productDialog = true;
-    }
-
-    editProduct(product: Product) {
-        this.product = { ...product };
-        this.productDialog = true;
-    }
-
-    deleteSelectedProducts() {
-        this.confirmationService.confirm({
-            message: 'Are you sure you want to delete the selected products?',
-            header: 'Confirm',
-            icon: 'pi pi-exclamation-triangle',
-            accept: () => {
-                this.products.set(this.products().filter((val) => !this.selectedProducts?.includes(val)));
-                this.selectedProducts = null;
-                this.messageService.add({
-                    severity: 'success',
-                    summary: 'Successful',
-                    detail: 'Products Deleted',
-                    life: 3000
-                });
-            }
+  deleteUser(user: User) {
+    this.confirmationService.confirm({
+      message: 'Are you sure you want to delete ' + user.userName + '?',
+      header: 'Confirm',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+        // TODO: Eliminacion de usuario API
+        this.user = { userName: '', id: 0, email: '', password: '' };
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Successful',
+          detail: 'User Deleted',
+          life: 3000
         });
+      }
+    });
+  }
+
+  saveUser() {
+    this.submitted = true;
+    let _users = this.users;
+
+    if (this.user.userName.trim().length === 0) return;
+
+    if (this.isNew) {
+      // TODO: Implementar API para nuevo usuario
+
+      this.messageService.add({
+        severity: 'success',
+        summary: 'Successful',
+        detail: 'User Created',
+        life: 3000
+      });
+    } else {
+      // TODO: Implementar API para actualizar usuario
+
+      let i = _users.findIndex(b => b.userName === this.unchangedUser.userName);
+      this.users[i] = this.user;
+
+      this.messageService.add({
+        severity: 'success',
+        summary: 'Successful',
+        detail: 'User Updated',
+        life: 3000
+      });
     }
 
-    hideDialog() {
-        this.productDialog = false;
-        this.submitted = false;
-    }
-
-    deleteProduct(product: Product) {
-        this.confirmationService.confirm({
-            message: 'Are you sure you want to delete ' + product.name + '?',
-            header: 'Confirm',
-            icon: 'pi pi-exclamation-triangle',
-            accept: () => {
-                this.products.set(this.products().filter((val) => val.id !== product.id));
-                this.product = {};
-                this.messageService.add({
-                    severity: 'success',
-                    summary: 'Successful',
-                    detail: 'Product Deleted',
-                    life: 3000
-                });
-            }
-        });
-    }
-
-    findIndexById(id: string): number {
-        let index = -1;
-        for (let i = 0; i < this.products().length; i++) {
-            if (this.products()[i].id === id) {
-                index = i;
-                break;
-            }
-        }
-
-        return index;
-    }
-
-    createId(): string {
-        let id = '';
-        var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-        for (var i = 0; i < 5; i++) {
-            id += chars.charAt(Math.floor(Math.random() * chars.length));
-        }
-        return id;
-    }
-
-    getSeverity(status: string) {
-        switch (status) {
-            case 'INSTOCK':
-                return 'success';
-            case 'LOWSTOCK':
-                return 'warn';
-            case 'OUTOFSTOCK':
-                return 'danger';
-            default:
-                return 'info';
-        }
-    }
-
-    saveProduct() {
-        this.submitted = true;
-        let _products = this.products();
-        if (this.product.name?.trim()) {
-            if (this.product.id) {
-                _products[this.findIndexById(this.product.id)] = this.product;
-                this.products.set([..._products]);
-                this.messageService.add({
-                    severity: 'success',
-                    summary: 'Successful',
-                    detail: 'Product Updated',
-                    life: 3000
-                });
-            } else {
-                this.product.id = this.createId();
-                this.product.image = 'product-placeholder.svg';
-                this.messageService.add({
-                    severity: 'success',
-                    summary: 'Successful',
-                    detail: 'Product Created',
-                    life: 3000
-                });
-                this.products.set([..._products, this.product]);
-            }
-
-            this.productDialog = false;
-            this.product = {};
-        }
-    }
+    this.userDialog = false;
+    this.user = { userName: '', id: 0, email: '', password: '' };
+  }
 }
